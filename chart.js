6e34c8ee8e0ccb2c04d1a8f7c81698f7fc3fba1b
woonees2 <hello_world_@kakao.com>
2022-02-12 15:22:37 +0900

    Add Chart

---
+++ chart.js
@@ -0,0 +1,75 @@
+const date = patientByDate.map(i => i.date.slice(5));
+const domesticPatient = patientByDate.map(i => i.domestic);
+const abroadPatient = patientByDate.map(i => i.abroad);
+const domesticDead = patientByDate.map(i => i.dead);
+const ctx = document.getElementById("patient-chart").getContext("2d");
+
+const red = "#B00323";
+const darkRed = "#580111";
+
+let chartData = {
+  labels: date,
+  datasets: [
+    {
+      label: "확진자 수",
+      yAxisID: "y-left",
+      data: domesticPatient,
+      backgroundColor: red,
+      borderColor: red,
+      borderWidth: 2,
+    },
+
+    {
+      label: "국내 사망자 수",
+      yAxisID: "y-right",
+      data: domesticDead,
+      backgroundColor: darkRed,
+      borderColor: darkRed,
+      borderWidth: 2,
+    },
+  ],
+};
+
+let chartOptions = {
+  responsive: true,
+
+  scales: {
+    x: {
+      title: {
+        display: true,
+        text: "날짜별 코로나 확진자 및 사망자(2022년)",
+      },
+    },
+    "y-left": {
+      type: "linear",
+      position: "left",
+      title: {
+        display: true,
+        text: "확진자 수",
+        color: red,
+      },
+      grid: {
+        display: false,
+      },
+    },
+    "y-right": {
+      type: "linear",
+      position: "right",
+      title: {
+        display: true,
+        text: "국내 사망자 수",
+        color: darkRed,
+      },
+
+      grid: {
+        display: false,
+      },
+    },
+  },
+};
+
+const myChart = new Chart(ctx, {
+  type: "line",
+  data: chartData,
+  options: chartOptions,
+});
