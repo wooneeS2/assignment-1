bff7af670320e20997f59698987eb2df78a749a3
woonees2 <hello_world_@kakao.com>
2022-02-21 19:39:48 +0900

    Update git ignore

--- task1/title.js
+++ task1/title.js
@@ -1,5 +1,5 @@
 const chevron = document.getElementById("chevron-down");
-const nextContent = document.getElementById("map-text-box1").offsetTop - 100;
+const nextContent = document.getElementById("map-text-box1").offsetTop;
 
 function handleChevronClick() {
   window.scroll({ top: nextContent, behavior: "smooth" });
